600c3f34c66c3c9ebae47fa287c893161a1103bd
Greg P <88697977+v-9x@users.noreply.github.com>
2025-04-03 14:52:46 -0700

    Formatting

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,10 +1,10 @@
-
 # hw1: CSCD 330, homework 1.
 # author: Greg Pappas
 
 from sys import argv
 from random import seed, randint
 import re
+
 
 def main():
     # Do not edit main.
@@ -32,15 +32,14 @@ def main():
             text += line
             line_count += 1
 
-    print_line_after(text, randint(0, line_count - 1),
-                     randint(0, max_value))
+    print_line_after(text, randint(0, line_count - 1), randint(0, max_value))
 
 
 def sum_every_nth(values, n):
     """
     Prints out the sum of every nth element of values, including
-    the 0th element. 
-    For example: 
+    the 0th element.
+    For example:
         - ([1, 2, 3, 4], 2) => 4
         - ([3, 5], 7) => 3
 
@@ -51,15 +50,15 @@ def sum_every_nth(values, n):
         - None (Output is printed to STDOUT)
     """
     sum = 0
-    for i in range(0,len(values),n):
-      sum += values[i]
+    for i in range(0, len(values), n):
+        sum += values[i]
     print(sum)
 
 
 def count_duplicates(values):
     """
     Prints out a sorted list of [(value, occurence_count)] calculated
-    from the input values. 
+    from the input values.
     For example:
         - ([1, 2, 3]) => [(1, 1), (2, 1), (3, 1)]
         - ([1, 2, 3, 2, 1]) => [(1, 2), (2, 2), (3, 1)]
@@ -70,12 +69,12 @@ def count_duplicates(values):
         - None (Output is printed to STDOUT)
     """
     duplicates = {x: 1 for x in values}
-    for i in range(0,len(values) - 1):
-      dups = 0
-      for j in range(i + 1,len(values)):
-        if values[i] == values[j]:
-          dups = dups + 1
-      duplicates[values[i]] = duplicates[values[i]] + dups
+    for i in range(0, len(values) - 1):
+        dups = 0
+        for j in range(i + 1, len(values)):
+            if values[i] == values[j]:
+                dups = dups + 1
+        duplicates[values[i]] = duplicates[values[i]] + dups
     print(sorted(duplicates.items()))
 
 
@@ -85,7 +84,7 @@ def print_line_after(text, line, after):
     For example:
         - ("Hello, world!", 0, 7) => world!
         - ("1\n2\n3", 1, 0) => 2
-        - ("1\n2\n3", 1, 5) => 
+        - ("1\n2\n3", 1, 5) =>
 
     Inputs:
         - text (String): A string of characters.
@@ -96,12 +95,11 @@ def print_line_after(text, line, after):
     Outputs:
         - None (Output is printed to STDOUT)
     """
-    if (line > 0):
-      splitted = text.split('\n')
-      print(splitted[line][after:])
+    if line > 0:
+        splitted = text.split("\n")
+        print(splitted[line][after:])
     else:
-      print(text[after:])
-
+        print(text[after:])
 
 
 if __name__ == "__main__":
