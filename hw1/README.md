87db108576ea7c9afeb4c316875cf7c01c1b1755
Greg P <88697977+v-9x@users.noreply.github.com>
2025-04-03 10:32:54 -0700

    Init commit

---
+++ hw1/README.md
@@ -0,0 +1,16 @@
+# HW1:
+In future assignments, we expect you to create the README. (Either a
+README.txt, a README.md, or a README (no file extension).) We are expecting the
+following three sections minimum. Please answer the questions from the
+directions below.
+
+## Usage:
+- python3 hw1.py seed count max
+
+## Examples:
+- python3 hw1.py 1 100 10
+- python3 hw1.py 2 100 10
+- python3 hw1.py 3 1000 50
+
+## Program description
+This program performs various string functions on the text Frankenstein.
---
+++ hw1/hw1.py
@@ -0,0 +1,99 @@
+
+# hw1: CSCD 330, homework 1.
+# author: Greg Pappas
+
+from sys import argv
+from random import seed, randint
+
+
+def main():
+    # Do not edit main.
+
+    if len(argv) != 4:
+        print("Usage:")
+        print("\tpython3 hw1.py seed count max")
+        exit(1)
+
+    # Convert from string to int.
+    seed_value, element_count, max_value = map(int, argv[1:])
+    seed(seed_value)  # Output must be reproducible.
+
+    values = []
+    for _ in range(element_count):
+        values += [randint(0, max_value)]
+
+    sum_every_nth(values, randint(1, max_value))
+    count_duplicates(values)
+
+    text = ""
+    line_count = 0
+    with open("Frankenstein.txt", "r") as file:
+        for line in file:  # Do not modify.
+            text += line
+            line_count += 1
+
+    print_line_after(text, randint(0, line_count - 1),
+                     randint(0, max_value))
+
+
+def sum_every_nth(values, n):
+    """
+    Prints out the sum of every nth element of values, including
+    the 0th element. 
+    For example: 
+        - ([1, 2, 3, 4], 2) => 4
+        - ([3, 5], 7) => 3
+
+    Inputs:
+        - values (int[]): A list of ints.
+        - n (int): An int to step by.
+    Outputs:
+        - None (Output is printed to STDOUT)
+    """
+    sum = 0
+    for i in range(0,len(values),n):
+      sum += values[i]
+    print(sum)
+
+
+def count_duplicates(values):
+    """
+    Prints out a sorted list of [(value, occurence_count)] calculated
+    from the input values. 
+    For example:
+        - ([1, 2, 3]) => [(1, 1), (2, 1), (3, 1)]
+        - ([1, 2, 3, 2, 1]) => [(1, 2), (2, 2), (3, 1)]
+
+    Inputs:
+        - values (int): A list of ints.
+    Outputs:
+        - None (Output is printed to STDOUT)
+    """
+    duplicates = {}
+    # TODO: Use a dictionary and a for loop.
+    print(sorted(duplicates.items()))
+
+
+def print_line_after(text, line, after):
+    """
+    Print the line from text starting at the index 'after'.
+    For example:
+        - ("Hello, world!", 0, 7) => world!
+        - ("1\n2\n3", 1, 0) => 2
+        - ("1\n2\n3", 1, 5) => 
+
+    Inputs:
+        - text (String): A string of characters.
+        - line (int): An int selecting the line from the text as
+                      separated by the new line character '\n'
+        - after (int): An int selecting the starting index on the line
+                       to print. Inclusive.
+    Outputs:
+        - None (Output is printed to STDOUT)
+    """
+    # TODO: Use the split function.
+    pass
+
+
+if __name__ == "__main__":
+    main()
