c9f74e9a1c0636a5b4d218d537f7111c32aa0275
Greg P <88697977+v-9x@users.noreply.github.com>
2025-04-21 16:53:43 -0700

    Add caching

--- Lab3/flask_example.py
+++ Lab3/flask_example.py
@@ -9,70 +9,93 @@ from subprocess import getstatusoutput
 
 app = Flask(__name__)
 
+addressCache = {}
+weatherCache = {}
+rangeCache = {}
+
 
 @app.route(f"/address/<domain>")
 def address(domain):
-    host = gethostbyname(domain)
-    status, output = getstatusoutput(f"whois {host}")
-    splitted = output.splitlines()
+    if addressCache.get(domain) != None:
+        return addressCache.get(domain)
+    else:
+        host = gethostbyname(domain)
+        status, output = getstatusoutput(f"whois {host}")
+        splitted = output.splitlines()
 
-    city = ""
-    address = ""
-    state = ""
-    zip = ""
+        city = ""
+        address = ""
+        state = ""
+        zip = ""
 
-    for line in splitted:
-        if line.startswith("City"):
-            city = line.partition("City")[2][1:].strip()
-        elif line.startswith("Address"):
-            address = line.partition("Address")[2][1:].strip()
-        # this is used for select websites.
-        elif line.startswith("StateProv"):
-            state = line.partition("StateProv")[2][1:].strip()
-        elif line.startswith("State"):
-            state = line.partition("State")[2][1:].strip()
-        elif line.startswith("PostalCode"):
-            zip = line.partition("PostalCode")[2][1:].strip()
+        for line in splitted:
+            if line.startswith("City"):
+                city = line.partition("City")[2][1:].strip()
+            elif line.startswith("Address"):
+                address = line.partition("Address")[2][1:].strip()
+            # this is used for select websites.
+            elif line.startswith("StateProv"):
+                state = line.partition("StateProv")[2][1:].strip()
+            elif line.startswith("State"):
+                state = line.partition("State")[2][1:].strip()
+            elif line.startswith("PostalCode"):
+                zip = line.partition("PostalCode")[2][1:].strip()
 
-    final = address + ", " + city + ", " + state + ", " + zip
-    return final
+        final = f"{address}, {city}, {state}, {zip}"
+        addressCache.update({domain: f"Cached: {final}"})
+        return final
 
 
 @app.route("/weather/<domain>")
 def weather(domain):
-    addressLine = address(domain)
-    weatherAdd = f"https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address={addressLine}&benchmark=4&format=json"
+    if weatherCache.get(domain) != None:
+        return weatherCache.get(domain)
+    else:
+        addressLine = address(domain)
+        weatherAdd = f"https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address={addressLine}&benchmark=4&format=json"
 
-    response = get(weatherAdd)
-    js = loads(response.text)
+        response = get(weatherAdd)
+        js = loads(response.text)
 
-    if js["result"]["addressMatches"] == []:
-        return "Domain does not provide sufficent data to get weather information. Try a different domain."
+        if js["result"]["addressMatches"] == []:
+            # I decided not to cache this response in case weather information does get added at any point in time.
+            return "Domain does not provide sufficent data to get weather information. Try a different domain."
 
-    x = js["result"]["addressMatches"][0]["coordinates"]["x"]
-    y = js["result"]["addressMatches"][0]["coordinates"]["y"]
+        x = js["result"]["addressMatches"][0]["coordinates"]["x"]
+        y = js["result"]["addressMatches"][0]["coordinates"]["y"]
 
-    weather_s = f"https://api.weather.gov/points/{y},{x}"
-    response = get(weather_s)
-    js = loads(response.text)
+        weather_s = f"https://api.weather.gov/points/{y},{x}"
+        response = get(weather_s)
+        js = loads(response.text)
 
-    forecast = js["properties"]["forecast"]
-    response = get(forecast)
-    js = loads(response.text)
+        forecast = js["properties"]["forecast"]
+        response = get(forecast)
+        js = loads(response.text)
 
-    currentForecast = js["properties"]["periods"][0]["detailedForecast"]
-    return currentForecast
+        currentForecast = js["properties"]["periods"][0]["detailedForecast"]
+
+        weatherCache.update({domain: f"Cached: {currentForecast}"})
+        return currentForecast
 
 
 @app.route("/range/<domain>")
 def range(domain):
-    host = gethostbyname(domain)
-    status, output = getstatusoutput(f"whois {host}")
-    splitted = output.splitlines()
+    if rangeCache.get(domain) != None:
+        return rangeCache.get(domain)
+    else:
+        host = gethostbyname(domain)
+        status, output = getstatusoutput(f"whois {host}")
+        splitted = output.splitlines()
+        netRange = ""
 
-    for line in splitted:
-        if line.startswith("NetRange"):
-            return f"Network range for {domain} is" + line.partition("NetRange")[2][1:]
+        for line in splitted:
+            if line.startswith("NetRange"):
+                netRange = (
+                    f"Network range for {domain} is {line.partition("NetRange")[2][1:]}"
+                )
+
+        rangeCache.update({domain: f"Cached: {netRange}"})
+        return netRange
 
 
 @app.route("/")
