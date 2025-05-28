d397c4b713593c79c56255c2d349b1bc6fd9ef07
Greg P. <88697977+v-9x@users.noreply.github.com>
2025-05-27 19:33:16 -0700

    Update code

--- lab7/lab7.py
+++ lab7/lab7.py
@@ -1,14 +1,23 @@
 # Author: Greg Pappas
 
-from scapy.all import *
+from scapy.all import *  # type: ignore
 from socket import gethostbyname
 from sys import argv
 from subprocess import getstatusoutput
 
 domain = argv[1]
 max_hops = int(argv[2])
+as_nums = []
+as_str = ""
+
+ip = IP()  # type: ignore
+
+tcp = TCP()  # type: ignore
+tcp.flags = "S"
+
 try:
     ip_add = gethostbyname(domain)
+    ip.dst = ip_add
 except:
     print(f"{domain} could not be resolved. try another domain.")
     exit(1)
@@ -16,16 +25,7 @@ except:
 route = f"route to {domain} ({ip_add}), {max_hops} hops max"
 print(route)
 
-dport = 80
 
-ip = IP()
-ip.dst = ip_add
-
-tcp = TCP()
-tcp.dport = dport
-tcp.flags = "S"
-as_nums = []
-as_str = ""
 for i in range(1, max_hops + 1):
     ip.ttl = i
     packet = ip / tcp
@@ -45,8 +45,12 @@ for i in range(1, max_hops + 1):
 
         if as_nums.count(as_num) == 0:
             as_nums.append(as_num)
+
         if response.src == ip_add:
             break
+
 for i in range(len(as_nums)):
     as_str += f"AS{as_nums[i]} -> "
-print(f"Traversed AS numbers: {as_str[:-4]}")
+
+if len(as_nums) > 0:
+    print(f"Traversed AS numbers: {as_str[:-4]}")
